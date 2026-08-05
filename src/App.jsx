8471fefeac0c8e5f5aa80cdaeae2f7348e9f7b8f
Ankitadd11 <ankitadd11@gmail.com>
2026-08-05 12:09:34 +0530

    feat: projecgt setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main className="app-container">
+      <h1>Interview Question Bank</h1>
+
+      <p>
+        React application setup is working.
+      </p>
+    </main>
+  );
+}
+
+export default App;
